e7ad1c9269ce6821353335c221f782ccc8e086cd
Hugo Devillers <hugo.devillers@gmail.com>
2021-07-22 11:37:55 +0200

    Parse and return and swiss.Entry object.

--- swiss/entry.go
+++ swiss/entry.go
@@ -1,5 +1,9 @@
 package swiss
 
+import (
+	"fmt"
+)
+
 type Entry struct {
 	Access   string
 	Name     string
@@ -9,4 +13,15 @@ type Entry struct {
 	Phylum   string
 	Sequence string
 	Evidence int
+}
+
+func (e *Entry) Info() {
+	fmt.Printf("Accession:\t%s\n", e.Access)
+	fmt.Printf("Gene name:\t%s\n", e.Name)
+	fmt.Printf("Locus tag:\t%s\n", e.Locus)
+	fmt.Printf("Description:\t%s\n", e.Desc)
+	fmt.Printf("Organism:\t%s\n", e.Organism)
+	fmt.Printf("Phylum: \t%s\n", e.Phylum)
+	fmt.Printf("Evidence:\t%d\n", e.Evidence)
+	fmt.Printf("Sequence:\t%s\n", e.Sequence)
 }
--- swiss/reader.go
+++ swiss/reader.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"os"
 	"regexp"
+	"strconv"
 
 	gzip "github.com/klauspost/pgzip"
 )
@@ -78,10 +79,11 @@ func (r *Reader) Next() bool {
 		for r.scanner.Scan() {
 			// We suppose no error if line 1 is ok
 			line = r.scanner.Text()
-			r.data = append(r.data, line)
 			if r.reend.MatchString(line) {
 				return true
 			}
+			// Do not append the // line!
+			r.data = append(r.data, line)
 		}
 	}
 
@@ -101,7 +103,51 @@ func (r *Reader) Parse() *Entry {
 	// Initialize the new entry
 	var entry Entry
 
-	// Split data by line types
+	// Split data by line types into a map
+	mdata := make(map[string]string)
+	for _, line := range r.data {
+		key := line[0:2]
+		mdata[key] += line[5:]
+	}
+
+	// Retrieve accession number
+	// NOTE: For sake of simplicify, only the first accession will be kept
+	ac := regexp.MustCompile(`;\s?`).Split(mdata["AC"], -1)
+	entry.Access = ac[0]
+
+	// Retieve gene name and locus tag
+	if mdata["GN"] != "" {
+		// Retrieve the gene name (can be null)
+		gn := regexp.MustCompile(`Name=(\w+)`).FindStringSubmatch(mdata["GN"])
+		if gn != nil {
+			entry.Name = gn[1]
+		}
+
+		// Retrieve the locus tag (can be null)
+		lt := regexp.MustCompile(`OrderedLocusNames=([\w\-]+)`).FindStringSubmatch(mdata["GN"])
+		if lt != nil {
+			entry.Locus = lt[1]
+		}
+	}
+
+	// Retrieve the functional annotation
+	// NOTE: we suppose that all DE entries start with "RecName: Full="
+	if mdata["DE"] != "" {
+		de := regexp.MustCompile(`;`).Split(mdata["DE"], 2)
+		if regexp.MustCompile(`^RecName\: Full=`).MatchString(de[0]) {
+			entry.Desc = de[0][14:]
+		}
+	}
+
+	// Organisme and phylum
+	entry.Organism = mdata["OS"]
+	entry.Phylum = mdata["OC"]
+
+	// Protein sequence
+	entry.Sequence = regexp.MustCompile(`\s`).ReplaceAllString(mdata["  "], "")
+
+	// Entry evidence level
+	entry.Evidence, _ = strconv.Atoi(mdata["PE"][0:1])
 
 	return &entry
 }
