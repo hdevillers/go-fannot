4efb2942e94270109169a296535f716fe2281e2a
Hugo Devillers <hugo.devillers@gmail.com>
2021-07-22 15:41:31 +0200

    Testing evidence and taxonomy from Entry object.

--- swiss/entry.go
+++ swiss/entry.go
@@ -2,6 +2,7 @@ package swiss
 
 import (
 	"fmt"
+	"regexp"
 )
 
 type Entry struct {
@@ -12,7 +13,7 @@ type Entry struct {
 	Organism string
 	Phylum   string
 	Sequence string
-	Evidence int
+	Evidence string
 }
 
 func (e *Entry) Info() {
@@ -22,6 +23,43 @@ func (e *Entry) Info() {
 	fmt.Printf("Description:\t%s\n", e.Desc)
 	fmt.Printf("Organism:\t%s\n", e.Organism)
 	fmt.Printf("Phylum: \t%s\n", e.Phylum)
-	fmt.Printf("Evidence:\t%d\n", e.Evidence)
+	fmt.Printf("Evidence:\t%s\n", e.Evidence)
 	fmt.Printf("Sequence:\t%s\n", e.Sequence)
+}
+
+func (e *Entry) TestEvidence(re string) bool {
+	retest, err := regexp.Compile(re)
+	if err != nil {
+		panic("[TestEvidence]: Cannot compile regex.")
+	}
+
+	if retest.MatchString(e.Evidence) {
+		return true
+	}
+
+	return false
+}
+
+func (e *Entry) TestTaxonomy(re string) bool {
+	retest, err := regexp.Compile(re)
+	if err != nil {
+		panic("[TestTaxonomy]: Cannot compile regex.")
+	}
+
+	// First test the Organism string
+	if retest.MatchString(e.Organism) {
+		return true
+	}
+
+	// Then test the phylum (if previous test is false)
+	if retest.MatchString(e.Phylum) {
+		return true
+	}
+
+	return false
+}
+
+func (e *Entry) Test(tk, ts, ek, es string) bool {
+	// Skip direct
+	return false
 }
