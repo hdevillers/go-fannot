44c2b47abf1177ddc0ddc274e48d2ab1723641fb
Hugo Devillers <hugo.devillers@inrae.fr>
2022-04-04 21:17:13 +0200

    Add a few tests.

--- fannot/param_test.go
+++ fannot/param_test.go
@@ -22,6 +22,23 @@ func TestParamDefault(t *testing.T) {
 	if len(p.Rules) != 2 {
 		t.Errorf("Expecting 2 default rules, found %d", len(p.Rules))
 	}
+
+	// Check a given rule
+	if p.Rules[0].Cpy_gen != CPY_GEN_HIGH {
+		t.Error("Gene copy should be activated for the first default rule.")
+	}
+	if p.Rules[0].Min_sim != MIN_SIM_HIGH {
+		t.Errorf("Expected a minimal similarity of %.02f, found a similarity of %.02f.", MIN_SIM_HIGH, p.Rules[0].Min_sim)
+	}
+	if p.Rules[0].Min_lra != MIN_LRA_HIGH {
+		t.Errorf("Expected a minimal length ration  of %.02f, found a similarity of %.02f.", MIN_LRA_HIGH, p.Rules[0].Min_lra)
+	}
+	if p.Rules[0].Hit_sta != HIT_STA_HIGH {
+		t.Error("Wrong hit status for the first default rule.")
+	}
+	if p.Rules[1].Hit_sta != HIT_STA_NORM {
+		t.Error("Wrong hit status for the second default rule.")
+	}
 }
 
 // Test to read the three rules JSON (in example)
