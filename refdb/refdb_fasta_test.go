0ec0aa55eff14992eed5418e955f242323496b93
Hugo Devillers <hugo.devillers@gmail.com>
2024-06-24 13:44:56 +0200

    Functional tests to create refdb from fasta (Finished).

--- refdb/refdb_fasta_test.go
+++ refdb/refdb_fasta_test.go
@@ -42,12 +42,39 @@ func TestExtractRefFasta(t *testing.T) {
 func TestRefFastaToRefDB(t *testing.T) {
 	if os.Getenv("DO_PYTHON_TESTS") == "yes" {
 		input := "../examples/from_fasta/test.fasta"
+		outdir := "../examples/refdb/"
+		name := "TEST_FASTA"
+		desc := "Copy of an existing annotation"
 
-		_, err := os.Stat(input)
+		// Delete possible data from a previous build
+		_, err := os.Stat(outdir + name)
+		if err == nil {
+			err = os.RemoveAll(outdir + name)
+			if err != nil {
+				t.Fatal("Failed to remove data from previous tests.")
+			}
+		}
+
+		// Check if the input exists
+		_, err = os.Stat(input)
 		if err != nil {
 			t.Fatal("Failed to find input fasta file.")
 		}
 
+		// Init. the Refdb object
+		rdb_ff := NewRefdb(outdir, name, input, desc, false, false, true)
+
+		// Load the data
+		rdb_ff.LoadFasta()
+
+		// Save the JSON
+		rdb_ff.WriteJson()
+
+		// Check files
+		_, err = os.Stat(outdir + name + "/config.json")
+		if err != nil {
+			t.Fatal("Failed to create the refdb from the reference FASTA file.")
+		}
 	} else {
 		// The test is skipped if DO_PYTHON_TEST is different from "yes" or unset
 		t.Skip("Tests using Python are disabled by default, to enable it, use 'make test do_python_tests=yes'.")
